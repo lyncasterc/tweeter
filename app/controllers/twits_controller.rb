652caaa33beb0e5c00ce5e98686c5e933eb4fe55
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-31 19:14:55 -0400

    refactor index action in TwitsController

--- app/controllers/twits_controller.rb
+++ app/controllers/twits_controller.rb
@@ -3,7 +3,8 @@ class TwitsController < ApplicationController
 
   # GET /twits or /twits.json
   def index
-    @twits = Twit.all
+    @twits = Twit.all.order(created_at: :desc)
+    @twit = Twit.new
   end
 
   # GET /twits/1 or /twits/1.json
