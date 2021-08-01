9af47d73e776769a2482985d4dbc66ed597514bf
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-07-31 23:27:42 -0400

    add user_id column to twits table

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_01_011911) do
+ActiveRecord::Schema.define(version: 2021_08_01_032630) do
 
   create_table "twits", force: :cascade do |t|
     t.text "twit"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
