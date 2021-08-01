da3d12c81cd529f2c554750980080a59a3352c56
stivencabrera3 <stiven.cabrera3@gmail.com>
2021-08-01 14:48:32 -0400

    refactoring

--- app/controllers/twits_controller.rb
+++ app/controllers/twits_controller.rb
@@ -14,7 +14,7 @@ class TwitsController < ApplicationController
 
   # GET /twits/new
   def new
-    @twit = Twit.new
+    @twit = current_user.twits.build
   end       
 
   # GET /twits/1/edit
@@ -23,7 +23,7 @@ class TwitsController < ApplicationController
 
   # POST /twits or /twits.json
   def create
-    @twit = Twit.new(twit_params)
+    @twit = current_user.twits.build(twit_params)
 
     respond_to do |format|
       if @twit.save
